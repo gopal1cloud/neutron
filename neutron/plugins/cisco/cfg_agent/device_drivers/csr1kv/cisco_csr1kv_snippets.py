6c102becb2cf0751e70f14367cdb5fd6931dbb24
npraveen35 <praveenn@onecloudinc.com>
2015-10-08 18:07:21 +0530

    Applied PEP8 compliants(asr1k_snippets.py and cisco_csr1kv_snippets.py files) for csr1kv folder

--- neutron/plugins/cisco/cfg_agent/device_drivers/csr1kv/cisco_csr1kv_snippets.py
+++ neutron/plugins/cisco/cfg_agent/device_drivers/csr1kv/cisco_csr1kv_snippets.py
@@ -193,7 +193,7 @@ SNAT_CFG = "ip nat inside source list %s interface %s vrf %s overload"
 SET_DYN_SRC_TRL_INTFC = """
 <config>
         <cli-config-data>
-            <cmd>ip nat inside source list %s interface %s vrf %s overload</cmd>
+            <cmd>ip nat inside source list %s interface %s vrf %s overload</cmd>  # noqa
         </cli-config-data>
 </config>
 
@@ -209,7 +209,7 @@ SET_DYN_SRC_TRL_INTFC = """
 REMOVE_DYN_SRC_TRL_INTFC = """
 <config>
         <cli-config-data>
-            <cmd>no ip nat inside source list %s interface %s vrf %s overload</cmd>
+            <cmd>no ip nat inside source list %s interface %s vrf %s overload</cmd>  # noqa
         </cli-config-data>
 </config>
 
@@ -346,7 +346,3 @@ CLEAR_DYN_NAT_TRANS = """
         </cli-config-data>
 </config>
 """
-
-
-
-
